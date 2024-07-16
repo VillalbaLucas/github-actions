08737b6fa3b65b4ef294feed400b1d6ba9ea7b49
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-07-16 00:04:44 -0300

    add: github action

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import {v1 as uuidv1} from 'uuid'
+
 
 const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GitHub Actions!')
+})
+
+app.get('/actions', (req, res) => {
+    res.json({
+        'id': uuidv1(),
+        'actions': 'UUID'
+    })
 })
 
 app.listen(PORT, (req, res) => {
