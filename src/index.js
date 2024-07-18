d7bb33a5ad3efeba36ab0a105246bae3bc196a88
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-07-17 22:52:38 -0300

    add: test

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from "./main.js";
+
+const PORT = 3000
+
+app.listen(PORT, (req, res) => {
+    console.log(`Listen in port localhost:${PORT}`);
+})
+
--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import express from 'express';
 import { indexRouter } from './routes/index.js';
 
-
-const PORT = 3000;
 const app = express();
 
 app.use('/', indexRouter);
 
-app.listen(PORT, (req, res) => {
-    console.log(`Listen in port localhost:${PORT}`);
-})
-
+export default app;
